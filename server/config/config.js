a6da1b262b32f3ddb8f1dc7bafe7cebffa7a215f
Ignacio Giralte <ignaciogiralte@gmail.com>
2018-08-01 11:13:21 +0200

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -23,7 +23,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
   urlDB ='mongodb://localhost:27017/cafe';
 }else{
-   urlDB = 'mongodb://cafe-user:nuevayork2018@ds261521.mlab.com:61521/cafe';
+   urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
